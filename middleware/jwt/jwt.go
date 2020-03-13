362522570247e16ab7e4d684c3821b8f965bd558
huhao <915664508@qq.com>
2020-03-13 19:41:20 +0800

    chore: 将 token 放入请求头进行鉴权

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -1,30 +1,33 @@
 package jwt
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/gin-gonic/gin"
-
 	"github.com/huhaophp/eblog/pkg/e"
 	"github.com/huhaophp/eblog/pkg/util"
+	"net/http"
+	"strings"
+	"time"
 )
 
 func JWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var code int
 		var data interface{}
-
 		code = e.SUCCESS
-		token := c.Query("token")
-		if token == "" {
+		authorization := c.GetHeader("Authorization")
+		if authorization == "" {
 			code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
 		} else {
-			claims, err := util.ParseToken(token)
-			if err != nil {
+			bearers := strings.Split(authorization, " ")
+			if bearers[1] == "" {
 				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
-			} else if time.Now().Unix() > claims.ExpiresAt {
-				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			} else {
+				claims, err := util.ParseToken(bearers[1])
+				if err != nil {
+					code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+				} else if time.Now().Unix() > claims.ExpiresAt {
+					code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+				}
 			}
 		}
 
